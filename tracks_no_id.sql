d872d132099cf8452966b42af963845a1eee65b2
Rico <richardthomaswhitfield@gmail.com>
2017-02-15 21:08:11 -0600

    new sql queries

--- tracks_no_id.sql
+++ tracks_no_id.sql
@@ -1,2 +1,8 @@
-select Track.Name, Album.Title, MediaType.name, Genre.name
-from Track, Album, MediaType, Genre
+select al.Title 'Album', t.Name 'Track', m.Name 'Media Type', g.Name 'Genre'
+from Track t
+inner join Album al 
+on al.AlbumId == t.AlbumId
+inner join MediaType m
+on m.MediaTypeId == t.MediaTypeId
+inner join Genre g
+on g.GenreId == t.GenreId
